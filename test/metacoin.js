00bd0d9c0135de65549402233391e4921835398a
HeegyoungChoi <heegyoung.choi@switchwon.com>
2023-01-27 23:48:35 -0500

    first commit

--- test/metacoin.js
+++ test/metacoin.js
@@ -1,22 +1,23 @@
 const MetaCoin = artifacts.require("MetaCoin");
 
 contract('MetaCoin', (accounts) => {
+  let metaCoinInstance;
+  beforeEach(async  () => {
+    metaCoinInstance = await MetaCoin.deployed();
+  });
+
   it('should put 10000 MetaCoin in the first account', async () => {
-    const metaCoinInstance = await MetaCoin.deployed();
     const balance = await metaCoinInstance.getBalance.call(accounts[0]);
 
     assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
   });
   it('should call a function that depends on a linked library', async () => {
-    const metaCoinInstance = await MetaCoin.deployed();
     const metaCoinBalance = (await metaCoinInstance.getBalance.call(accounts[0])).toNumber();
     const metaCoinEthBalance = (await metaCoinInstance.getBalanceInEth.call(accounts[0])).toNumber();
 
     assert.equal(metaCoinEthBalance, 2 * metaCoinBalance, 'Library function returned unexpected function, linkage may be broken');
   });
   it('should send coin correctly', async () => {
-    const metaCoinInstance = await MetaCoin.deployed();
-
     // Setup 2 accounts.
     const accountOne = accounts[0];
     const accountTwo = accounts[1];
@@ -38,8 +39,6 @@ contract('MetaCoin', (accounts) => {
   });
   // ---------------------- ADD TWO TEST CASE ---------------------- //
   it('should exactly the amount user sent have to be checked', async () => {
-    const metaCoinInstance = await MetaCoin.deployed();
-
     let balanceBeforeSend = (await metaCoinInstance.getBalance.call(accounts[1])).toNumber();
     let balanceBeforeReceive = (await metaCoinInstance.getBalance.call(accounts[3])).toNumber();
 
@@ -53,8 +52,6 @@ contract('MetaCoin', (accounts) => {
   });
 
   it('should balance 0 when get balance using not existing address', async () => {
-    const metaCoinInstance = await MetaCoin.deployed();
-
     var crypto = require('crypto');
     var randomAddress = "0x"+ crypto.randomBytes(20).toString('hex');;
 
